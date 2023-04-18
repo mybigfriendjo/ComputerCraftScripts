98f3cab2fa52a5819af13ee9506bc100747c0bb5
mybigfriendjo <mybigfriendjo@users.noreply.github.com>
2023-04-18 20:18:24 +0200

    extracted common methods to seperate tool library (mostly storage stuff)

---
+++ turtle/base/tools.lua
@@ -0,0 +1,35 @@
+local function tableHasValue(inputTable, valueToCheck)
+    for index, value in ipairs(inputTable) do
+        if value == valueToCheck then
+            return true
+        end
+    end
+    return false
+end
+
+local function isInventoryFull()
+    for i = 1, 16 do
+        if turtle.getItemCount(i) == 0 then
+            return false
+        end
+    end
+    return true
+end
+
+local function getSlotContaining(itemName)
+    local foundSlot = -1
+    for i = 1, 16 do
+        local itemData = turtle.getItemDetail(i)
+        if itemData ~= nil and itemData.name == itemName then
+            foundSlot = i
+            break
+        end
+    end
+    return foundSlot
+end
+
+return {
+    tableHasValue = tableHasValue,
+    isInventoryFull = isInventoryFull,
+    getSlotContaining = getSlotContaining
+}
