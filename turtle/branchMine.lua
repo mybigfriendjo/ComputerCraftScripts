8a1f72c821bddd2710000214da88c28381af1d7c
mybigfriendjo <mybigfriendjo@users.noreply.github.com>
2023-04-18 20:19:22 +0200

    adding capabilities to place cobblestone into lava; NOT DONE

--- turtle/branchMine.lua
+++ turtle/branchMine.lua
@@ -1,34 +1,98 @@
 local mov = require("base/mov")
+local tools = require("base/tools")
 
 local args = {...}
 
-local x = tonumber(args[1])
+-- handle lava insulation
+-- check full inventory
 
-local torchSlot = 16
+local branchLength = tonumber(args[1])
+local continueDirection = args[2]
+local continueAmount = tonumber(args[3])
 
-local function placeTorch()
-    local currentSelection = turtle.getSelectedSlot()
-    turtle.select(torchSlot)
-    turtle.placeDown()
-    turtle.select(currentSelection)
+if branchLength == nil then
+    print("needs branchLength")
+    return
 end
 
-turtle.dig()
-mov.f()
-turtle.digDown()
-placeTorch()
-
-for i = 1, x - 1 do
-    turtle.dig()
-    mov.f()
-    turtle.digDown()
-    if i % 10 == 0 then
-        placeTorch()
+if continueDirection ~= nil then
+    if continueAmount == nil then
+        print("needs continue amount")
+        return
     end
 end
 
-placeTorch()
+local cobbleBlocks = {
+    "minecraft:cobblestone",
+    "minecraft:netherrack"
+}
 
-for i = 1, x do
-    mov.b()
+local function placeTorch()
+    local torchSlot = tools.getSlotContaining("minecraft:torch")
+
+    if torchSlot ~= -1 then
+        local currentSelection = turtle.getSelectedSlot()
+        turtle.select(torchSlot)
+        turtle.placeDown()
+        turtle.select(currentSelection)
+    end
 end
+
+local function placeCobbleBlock(direction)
+    for i = 1, 16 do
+        local data = turtle.getItemDetail(i)
+        if data ~= nil and tools.tableHasValue(cobbleBlocks, data.name) then
+            local currentSelection = turtle.getSelectedSlot()
+            turtle.select(i)
+            if direction == "u" then
+                turtle.placeUp()
+            end
+            if direction == "d" then
+                turtle.placeDown()
+            end
+            if direction == "f" then
+                turtle.place()
+            end
+            turtle.select(currentSelection)
+        end
+    end
+end
+
+local function checkForLava()
+    local hasBlock, data
+    hasBlock, data = turtle.inspectUp()
+    if hasBlock then
+        if data ~= nil and data.name == "minecraft:lava" then
+            placeCobbleBlock("u")
+        end
+    end
+
+    -- TODO
+end
+
+local function digBranch()
+    turtle.dig()
+    mov.f()
+    turtle.digDown()
+    placeTorch()
+
+    for i = 1, branchLength - 1 do
+        turtle.dig()
+        mov.f()
+        turtle.digDown()
+
+        -- TODO
+
+        if i % 10 == 0 then
+            placeTorch()
+        end
+    end
+
+    placeTorch()
+
+    for i = 1, branchLength do
+        mov.b()
+    end
+end
+
+digBranch()
